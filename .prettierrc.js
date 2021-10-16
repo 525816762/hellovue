c4568750cc1a3a3869ecfcb988e6820b6d173077
525816762 <525816762@163.com>
2021-10-16 11:17:06 +0800

    代码格式化

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    semi: false, // 行尾分号
+    tabWidth: 4, // 缩进字节数
+    useTabs: false, // 缩进不使用tab，使用空格
+    singleQuote: true, // 使用单引号
+    printWidth: 100, // 一行最多 100 字符
+    trailingComma: 'none', // 末尾不需要逗号
+    arrowParens: 'avoid' //只有一个参数的箭头函数的参数是否带圆括号（默认avoid不带）
+}
